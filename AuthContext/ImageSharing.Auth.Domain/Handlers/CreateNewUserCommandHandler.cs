7cd0909859c0c5edb2cf41362dd83575fa945200
Gustavo Fontes <gustavo.ferrazfontes@quotationfactory.com>
2024-10-27 20:39:48 -0300

    refactor: simplify constructor

--- AuthContext/ImageSharing.Auth.Domain/Handlers/CreateNewUserCommandHandler.cs
+++ AuthContext/ImageSharing.Auth.Domain/Handlers/CreateNewUserCommandHandler.cs
@@ -7,26 +7,22 @@ using MediatR;
 
 namespace ImageSharing.Auth.Domain.Handlers;
 
-internal sealed class CreateNewUserCommandHandler : IRequestHandler<CreateNewUserCommand>
+internal sealed class CreateNewUserCommandHandler(
+    IUserEncryptService userEncryptService,
+    IPublishEndpoint publishEndpoint)
+    : IRequestHandler<CreateNewUserCommand>
 {
-    private readonly IUserEncryptService _userEncryptService;
-    private readonly IPublishEndpoint _publishEndpoint;
-
-    public CreateNewUserCommandHandler(IUserEncryptService userEncryptService, IPublishEndpoint publishEndpoint)
-    {
-        this._userEncryptService = userEncryptService;
-        this._publishEndpoint = publishEndpoint;
-    }
     public async Task Handle(CreateNewUserCommand request, CancellationToken cancellationToken)
     {
+        
         var avatarPath = "";
-        var salt = _userEncryptService.GenerateSalt();
-        var hashedPassword = _userEncryptService.HashPassword(request.Password, salt);
+        var salt = userEncryptService.GenerateSalt();
+        var hashedPassword = userEncryptService.HashPassword(request.Password, salt);
         var base64Salt = Convert.ToBase64String(salt);
 
         var newUser = new User(request.UserName, request.Email, avatarPath, hashedPassword, salt);
 
-        await _publishEndpoint.Publish(new CreatedUserEvent { Id = newUser.Id, Email = newUser.Email, UserName = newUser.UserName, Avatar = "" }, cancellationToken);
+        await publishEndpoint.Publish(new CreatedUserEvent { Id = newUser.Id, Email = newUser.Email, UserName = newUser.UserName, Avatar = "" }, cancellationToken);
 
     }
 }
