5d53420c30f8dad2d168438b0c4726dd8764abb0
Gustavo Fontes <gustavo.ferrazfontes@quotationfactory.com>
2024-10-29 21:41:01 -0300

    refactor: call create new user command into signin endpoint passing data from request payload

---
+++ AuthContext/ImageSharing.Auth.Api/Models/Auth/Requests/SignInRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ImageSharing.Auth.Api.Models.Auth.Requests;
+
+public record SignInRequest
+{
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+    public string? ConfirmPassword { get; set; }
+}
