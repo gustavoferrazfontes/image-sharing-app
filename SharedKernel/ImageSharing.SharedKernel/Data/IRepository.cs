1173e8a8742ad53472da0d1297ee5558f896a271
Gustavo Fontes <gustavo.ferrazfontes@gmail.com>
2024-10-29 21:36:22 -0300

    feat: create unit of work and repository base interfaces

---
+++ SharedKernel/ImageSharing.SharedKernel/Data/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+using ImageSharing.SharedKernel.Model;
+
+namespace ImageSharing.SharedKernel.Data;
+
+public interface IRepository<T> where T : IEntity
+{
+   Task AddAsync(T entity);    
+   Task<T?> GetByIdAsync(Guid id);
+   Task<IEnumerable<T>?> GetAllAsync(Expression<Func<T,bool>>? filters = null, string? includeProperties = null);
+   Task UpdateAsync(T entity);
+}
