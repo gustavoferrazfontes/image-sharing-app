8cf926cea853783921695659a05977abb3eba9b3
Gustavo Fontes <gustavo.ferrazfontes@quotationfactory.com>
2024-10-29 21:41:42 -0300

    refactor: improve User entity

--- AuthContext/ImageSharing.Auth.Domain/Models/User.cs
+++ AuthContext/ImageSharing.Auth.Domain/Models/User.cs
@@ -1,21 +1,30 @@
 ﻿
+using ImageSharing.SharedKernel.Model;
+
 namespace ImageSharing.Auth.Domain.Models;
 
-public class User
+public class User:IEntity
 {
-    public User(string userName, string email, string avatarPath, string hashedPassword, byte[] salt)
+    protected User(){}
+    
+    public User(string userName, string email, string avatarPath, string hashedPassword, string salt)
     {
+        Id = Guid.NewGuid();
         UserName = userName;
         Email = email;
         AvatarPath = avatarPath;
         HashedPassword = hashedPassword;
-        Salt = salt;
+        Base64Salt = salt;
+        CreatedAt = DateTime.Now;
     }
 
-    public string UserName { get; private set; }
-    public string Email { get; private set; }
-    public string AvatarPath { get;private set; }
-    public string HashedPassword { get; private set; }
-    public byte[] Salt { get; private set; }
-    public Guid Id { get; internal set; }
+    public Guid Id { get; private set; }
+    public string UserName { get; private set; } 
+    public string Email { get; private set; } 
+    public string AvatarPath { get;private set; } 
+    public string HashedPassword { get; private set; } 
+    public string Base64Salt { get; private set; } 
+    public bool IsActive { get; private  set; }
+    public bool IsDeleted { get;private set; }
+    public DateTime CreatedAt { get; private set; }
 }
