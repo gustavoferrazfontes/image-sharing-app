160e0274c85b47b1ccc5d9930f38be764c3750e3
Gustavo Fontes <gustavo.ferrazfontes@quotationfactory.com>
2024-10-29 21:37:35 -0300

    feat: create Login flow

---
+++ AuthContext/ImageSharing.Auth.Domain/Commands/LoginCommand.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+
+namespace ImageSharing.Auth.Domain.Commands;
+
+public class LoginCommand:IRequest<LoginResponse>
+{
+    public LoginCommand(string email, string password)
+    {
+        Email = email;
+        Password = password;
+    }
+
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
+
+public record LoginResponse
+{
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+}
---
+++ AuthContext/ImageSharing.Auth.Domain/Handlers/LoginCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using ImageSharing.Auth.Domain.Commands;
+using ImageSharing.Auth.Domain.Interfaces;
+using MediatR;
+
+namespace ImageSharing.Auth.Domain.Handlers;
+
+internal class LoginCommandHandler : IRequestHandler<LoginCommand, LoginResponse>
+{
+    private readonly IUserEncryptService _userEncryptService;
+    private readonly IUserRepository _userRepository;
+
+    public LoginCommandHandler(IUserEncryptService userEncryptService, IUserRepository userRepository)
+    {
+        _userEncryptService = userEncryptService;
+        _userRepository = userRepository;
+    }
+    public async Task<LoginResponse> Handle(LoginCommand request, CancellationToken cancellationToken)
+    {
+
+        var user = await _userRepository.GetByEmailAsync(request.Email);
+        if (user is null)
+            throw new Exception("user or password invalid");
+
+
+        var isMatch = _userEncryptService.IsMatch(user.Base64Salt, user.HashedPassword, request.Password);
+        if (!isMatch)
+            throw new Exception("user or password invalid");
+
+        return new LoginResponse
+        {
+            Email = user.Email,
+            Name = user.UserName
+        };
+
+    }
+}
