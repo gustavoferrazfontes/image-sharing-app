15a6e535ea2c7556ece7d19f03523325e648a7fc
Gustavo Fontes <gustavo.ferrazfontes@quotationfactory.com>
2024-10-29 21:38:30 -0300

    feat: create IUserRepository interface

---
+++ AuthContext/ImageSharing.Auth.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using ImageSharing.Auth.Domain.Models;
+using ImageSharing.SharedKernel.Data;
+
+namespace ImageSharing.Auth.Domain.Interfaces;
+
+public interface IUserRepository:IRepository<User>
+{
+   Task<User> GetByEmailAsync(string email); 
+   Task<bool> IsEmailExistAsync(string email);  
+}
