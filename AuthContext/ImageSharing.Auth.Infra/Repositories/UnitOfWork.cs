605680c8fcf3b5a883608aab049a6632b4f16337
Gustavo Fontes <gustavo.ferrazfontes@gmail.com>
2024-10-29 21:36:47 -0300

    feat: implement unit of work and user repository

---
+++ AuthContext/ImageSharing.Auth.Infra/Repositories/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using ImageSharing.Auth.Infra.EF;
+using ImageSharing.SharedKernel.Data;
+
+namespace ImageSharing.Auth.Infra.Repositories;
+
+public class UnitOfWork(AuthDbContext context) : IUnitOfWork
+{
+    public Task CommitAsync()
+    {
+        try
+        {
+            return context.SaveChangesAsync();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
