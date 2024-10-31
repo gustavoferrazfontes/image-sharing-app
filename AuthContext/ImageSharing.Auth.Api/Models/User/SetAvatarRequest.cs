37d219fe76d32f91623031be62d4c8c9a91f0307
Gustavo Fontes <gustavo.ferrazfontes@gmail.com>
2024-10-31 01:36:28 -0300

    feat: create command to set an avatar to an user

---
+++ AuthContext/ImageSharing.Auth.Api/Models/User/SetAvatarRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace ImageSharing.Auth.Api.Models.Auth.Requests;
+
+public class SetAvatarRequest
+{
+    public string? Avatar64Base { get; set; }
+    public string? ImageExtension { get; set; }
+}
---
+++ AuthContext/ImageSharing.Auth.Domain/Commands/SetUserAvatarCommand.cs
@@ -0,0 +1,11 @@
+﻿using CSharpFunctionalExtensions;
+using MediatR;
+
+namespace ImageSharing.Auth.Domain.Commands;
+
+public class SetUserAvatarCommand(Guid userId, string avatar64Base, string imageExtension) : IRequest<Result>
+{
+    public string Avatar { get; private set; } = avatar64Base;
+    public string ImageExtension { get;private  set; } = imageExtension;
+    public Guid UserId { get; private set; } =  userId;
+}
