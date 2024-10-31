febc2bce9977555ed3f5e78c194f3f14add2de32
Gustavo Fontes <gustavo.ferrazfontes@gmail.com>
2024-10-31 01:36:51 -0300

    feat: create endpoint to expose set avatar feature

---
+++ AuthContext/ImageSharing.Auth.Api/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using ImageSharing.Auth.Api.Models.Auth.Requests;
+using ImageSharing.Auth.Domain.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageSharing.Auth.Api.Controllers;
+
+[Route("api/users")]
+public class UserController(IMediator mediator) : ApiControllerBase
+{
+   private readonly IMediator _mediator = mediator;
+
+   [HttpPost("{id}/set-avatar")]
+   public async Task<IActionResult> SetAvatar([FromRoute] Guid id,[FromBody] SetAvatarRequest payload)
+   {
+      var command = new SetUserAvatarCommand(id,payload.Avatar64Base,payload.ImageExtension);
+      var result = await _mediator.Send(command);
+      return ResponseResult(result);
+   }
+}
