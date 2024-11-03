cb59c2f6da7b0073783e4ab41e9810062b9a1ef1
Gustavo Fontes <gustavo.ferrazfontes@gmail.com>
2024-11-03 18:20:19 -0300

    feat: create search schema

---
+++ Tools/ImageSharing.AzureSearch.Upload/CreatedUser.cs
@@ -0,0 +1,16 @@
+﻿using Azure.Search.Documents.Indexes;
+
+public class CreatedUser
+{
+    [SimpleField(IsKey = true, IsFilterable = true)]
+    public string? UserId { get; set; }
+
+    [SearchableField(IsFilterable = true, IsSortable = true)]
+    public string? UserName { get; set; }
+
+    [SearchableField(IsFilterable = true, IsSortable = true)]
+    public string? Email { get; set; }
+
+    [SimpleField()]
+    public string? AvatarUrl { get; set; }
+}
