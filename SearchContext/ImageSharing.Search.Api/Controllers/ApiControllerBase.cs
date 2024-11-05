ac4e0de540ae4abb59b5bb166999eee94fe9adc9
Gustavo Fontes <gustavo.ferrazfontes@gmail.com>
2024-11-04 23:19:54 -0300

    feat: create a controller base class in order to standardize api response

---
+++ SearchContext/ImageSharing.Search.Api/Controllers/ApiControllerBase.cs
@@ -0,0 +1,21 @@
+﻿using CSharpFunctionalExtensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageSharing.Search.Api.Controllers;
+
+public class ApiControllerBase: ControllerBase
+{
+      [NonAction]
+      protected IActionResult ResponseResult<T>(Result<T> result)
+      {
+          var response = new Response<object?>(result.IsSuccess ? result.Value:null, result.IsFailure ? result.Error : string.Empty, result.IsSuccess);
+         return result.IsSuccess ? Ok(response) : BadRequest(response);
+      }
+      protected IActionResult ResponseResult(Result result) 
+      {
+          var response = new Response<object?>(null,result.IsFailure? result.Error:string.Empty, result.IsSuccess);
+          return result.IsSuccess ? Ok(response) : BadRequest(response);
+      }
+}
+
+public record Response<T>(T Data, string Message, bool Success);
