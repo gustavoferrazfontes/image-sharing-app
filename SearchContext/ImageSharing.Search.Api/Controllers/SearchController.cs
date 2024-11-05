e9db35ea4b3abbc773a71b621b4da724299c39ef
Gustavo Fontes <gustavo.ferrazfontes@gmail.com>
2024-11-04 23:18:26 -0300

    feat: create SearchController.cs

---
+++ SearchContext/ImageSharing.Search.Api/Controllers/SearchController.cs
@@ -0,0 +1,25 @@
+﻿using ImageSharing.Search.Domain.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageSharing.Search.Api.Controllers;
+
+[Route("api/searches")]
+public class SearchController : ApiControllerBase 
+{
+    private readonly IMediator _mediator;
+
+    public SearchController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    [HttpGet("users")]
+    public async Task<IActionResult> Index([FromQuery] int pagaSize, string? lastResultId)
+    {
+        var query = new GetUsersQuery(pagaSize,lastResultId);
+        var result = await _mediator.Send(query);
+
+        return ResponseResult(result);
+
+    }
+}
