bde841ed35af2fea10eb45851b57d7b34e3d0c13
Gustavo Fontes <gustavo.ferrazfontes@gmail.com>
2024-11-04 23:13:08 -0300

    feat: create query to return a list of paginated users

---
+++ SearchContext/ImageSharing.Search.Domain/Handlers/GetUsersQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using CSharpFunctionalExtensions;
+using ImageSharing.Search.Domain.Interfaces;
+using ImageSharing.Search.Domain.Queries;
+using ImageSharing.SharedKernel.Data.Storage;
+using ImageSharing.SharedKernel.Model;
+using MediatR;
+
+namespace ImageSharing.Search.Domain.Handlers;
+
+public sealed class GetUsersQueryHandler(IStorageService storageService, IUserRepository repository)
+    : IRequestHandler<GetUsersQuery, Result<PaginatedResult<GetUsersQueryResponse>>>
+{
+    private readonly IStorageService _storageService = storageService;
+    private readonly IUserRepository _repository = repository;
+
+    public Task<Result<PaginatedResult<GetUsersQueryResponse>>> Handle(GetUsersQuery request, CancellationToken cancellationToken)
+    {
+         return _repository.GetPaginatedAsync(request.PageSize, request.LastResultId)
+            .Tap(item =>
+            {
+                item.Items?.ToList().ForEach(item =>
+                {
+                    var _ =  _storageService.TryGetblobSasUri(item.UserId,  out string url,new TimeSpan(0, 5, 0));
+                    item.AvatarUrl = url;
+                });
+            });
+    }
+}
---
+++ SearchContext/ImageSharing.Search.Domain/Queries/GetUsersQuery.cs
@@ -0,0 +1,19 @@
+﻿using CSharpFunctionalExtensions;
+using ImageSharing.SharedKernel.Model;
+using MediatR;
+
+namespace ImageSharing.Search.Domain.Queries;
+
+public sealed class GetUsersQuery(int pageSize, string? lastResultId) : IRequest<Result<PaginatedResult<GetUsersQueryResponse>>>
+{
+    public int PageSize { get; } = pageSize;
+    public string? LastResultId { get; } = lastResultId;
+}
+
+public record GetUsersQueryResponse
+{
+    public string? UserId { get; set; }
+    public string? UserName { get; set; }
+    public string? Email { get; set; }
+    public string? AvatarUrl { get; set; }
+}
