e71864e85bd0da54f0ea105149c46d2f2f08a313
Gustavo Fontes <gustavo.ferrazfontes@gmail.com>
2024-11-05 21:36:49 -0300

    feat: create structure to add new post

---
+++ PostsContext/ImageSharing.Posts.Domain/Commands/AddNewPostCommand.cs
@@ -0,0 +1,18 @@
+﻿using CSharpFunctionalExtensions;
+using MediatR;
+
+namespace ImageSharing.Posts.Domain.Commands;
+
+public class AddNewPostCommand:IRequest<Result>
+{
+    public AddNewPostCommand(Guid userId,string? subtitle, string[]? tags)
+    {
+        UserId = userId;
+        Subtitle = subtitle;
+        Tags = tags;
+    }
+
+    public string[]? Tags { get;  }
+    public string? Subtitle { get;  }
+    public Guid UserId { get;  }
+}
---
+++ PostsContext/ImageSharing.Posts.Domain/Handlers/AddNewPostCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using CSharpFunctionalExtensions;
+using ImageSharing.Posts.Domain.Commands;
+using ImageSharing.Posts.Domain.Interfaces;
+using ImageSharing.Posts.Domain.Models;
+using ImageSharing.SharedKernel.Data.Storage;
+using MediatR;
+
+namespace ImageSharing.Posts.Domain.Handlers;
+
+internal sealed class AddNewPostCommandHandler(IPostRepository postRepository)
+    : IRequestHandler<AddNewPostCommand, Result>
+{
+    public Task<Result> Handle(AddNewPostCommand request, CancellationToken cancellationToken)
+    {
+        var post = new Post(request.UserId,request.Subtitle,request.Tags);
+
+        return postRepository.AddAsync(post);
+    }
+}
