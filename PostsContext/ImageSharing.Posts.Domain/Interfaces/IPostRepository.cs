b078b8e90702ea3138a77bbaa15844a3a8dbd01c
Gustavo Fontes <gustavo.ferrazfontes@gmail.com>
2024-11-05 21:37:06 -0300

    feat: create IPostRepository.cs interface

---
+++ PostsContext/ImageSharing.Posts.Domain/Interfaces/IPostRepository.cs
@@ -0,0 +1,9 @@
+﻿using CSharpFunctionalExtensions;
+using ImageSharing.Posts.Domain.Models;
+
+namespace ImageSharing.Posts.Domain.Interfaces;
+
+internal interface IPostRepository
+{
+   Task<Result> AddAsync(Post post); 
+}
