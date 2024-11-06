a28f7d0a0011df042e5e1b3cdf8f5c0dafcdf471
Gustavo Fontes <gustavo.ferrazfontes@gmail.com>
2024-11-05 21:37:22 -0300

    feat: create Post.cs entity model

---
+++ PostsContext/ImageSharing.Posts.Domain/Models/Post.cs
@@ -0,0 +1,25 @@
+﻿using CSharpFunctionalExtensions;
+using ImageSharing.SharedKernel.Model;
+
+namespace ImageSharing.Posts.Domain.Models;
+
+internal class Post:IEntity
+{
+    protected Post(){}
+    
+    public Post(Guid userId,string? subtitle, string[]? tags)
+    {
+        Id = Guid.NewGuid();
+        UserId = userId;
+        Subtitle = subtitle;
+        Tags = tags;
+    }
+    
+
+    public Guid Id { get; private set; }
+    public Guid UserId { get; private set; }
+    public string? Subtitle { get; private set; }
+    public string[]? Tags { get; private set; }
+    
+    public string ImagePath { get; private set; }
+}
