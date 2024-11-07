b96d5c1bade142254f4901bee2998b8c9d73c0f9
Gustavo Fontes <gustavo.ferrazfontes@gmail.com>
2024-11-06 21:47:35 -0300

    feat: map Post entity

---
+++ PostsContext/ImageSharing.Posts.Infra/EF/Mapping/PostMap.cs
@@ -0,0 +1,18 @@
+﻿using ImageSharing.Posts.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+namespace ImageSharing.Posts.Infra.EF.Mapping;
+
+internal class PostMap: IEntityTypeConfiguration<Post>
+{
+    public void Configure(EntityTypeBuilder<Post> builder)
+    {
+        builder.ToTable("posts");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).HasColumnName("id");
+        builder.Property(x => x.UserId).HasColumnName("user_id").IsRequired();
+        builder.Property(x => x.Subtitle).HasColumnName("subtitle").HasMaxLength(200);
+        builder.Property(x => x.Tags).HasColumnName("tags").HasMaxLength(255);
+        builder.Property(x => x.ImagePath).HasColumnName("image_path").IsRequired();
+    }
+}
---
+++ PostsContext/ImageSharing.Posts.Infra/EF/PostDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ImageSharing.Posts.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImageSharing.Posts.Infra.EF;
+
+internal class PostDbContext:DbContext
+{
+     protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(Post).Assembly);
+        }
+    
+        public DbSet<Post> Posts { get; set; }  
+}
