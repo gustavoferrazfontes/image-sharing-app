be40262bd7f54f4afe1cd8bcb9d0aaf21ab15697
Gustavo Fontes <gustavo.ferrazfontes@gmail.com>
2024-11-06 21:49:15 -0300

    feat: call commit method in AddNewPostCommandHandler.cs

--- PostsContext/ImageSharing.Posts.Domain/Handlers/AddNewPostCommandHandler.cs
+++ PostsContext/ImageSharing.Posts.Domain/Handlers/AddNewPostCommandHandler.cs
@@ -2,18 +2,22 @@
 using ImageSharing.Posts.Domain.Commands;
 using ImageSharing.Posts.Domain.Interfaces;
 using ImageSharing.Posts.Domain.Models;
-using ImageSharing.SharedKernel.Data.Storage;
+using ImageSharing.SharedKernel.Data;
 using MediatR;
 
 namespace ImageSharing.Posts.Domain.Handlers;
 
-internal sealed class AddNewPostCommandHandler(IPostRepository postRepository)
+internal sealed class AddNewPostCommandHandler(IPostRepository postRepository,IUnitOfWork unitOfWork)
     : IRequestHandler<AddNewPostCommand, Result>
 {
-    public Task<Result> Handle(AddNewPostCommand request, CancellationToken cancellationToken)
+    public async Task<Result> Handle(AddNewPostCommand request, CancellationToken cancellationToken)
     {
         var post = new Post(request.UserId,request.Subtitle,request.Tags);
 
-        return postRepository.AddAsync(post);
+         await postRepository.AddAsync(post);
+
+         await unitOfWork.CommitAsync();
+         
+         return Result.Success();
     }
 }
