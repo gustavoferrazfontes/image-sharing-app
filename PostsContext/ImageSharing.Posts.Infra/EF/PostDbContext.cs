4609caef0263e3491f8406e1bc1c2b46065ff027
Gustavo Fontes <gustavo.ferrazfontes@gmail.com>
2024-11-07 21:42:47 -0300

    feat: setup mapping entity classes in PostDbContext.cs

--- PostsContext/ImageSharing.Posts.Infra/EF/PostDbContext.cs
+++ PostsContext/ImageSharing.Posts.Infra/EF/PostDbContext.cs
@@ -1,14 +1,19 @@
 ﻿using ImageSharing.Posts.Domain.Models;
+using ImageSharing.Posts.Infra.EF.Mapping;
 using Microsoft.EntityFrameworkCore;
 
 namespace ImageSharing.Posts.Infra.EF;
 
 internal class PostDbContext:DbContext
 {
+    public PostDbContext(DbContextOptions<PostDbContext> options):base(options)
+    {
+        
+    }
      protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.ApplyConfigurationsFromAssembly(typeof(Post).Assembly);
-        }
+     {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(PostMap).Assembly);
+     }
     
         public DbSet<Post> Posts { get; set; }  
 }
