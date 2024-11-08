eefbfd859f06f654744102d21a44d37b1e90d988
Gustavo Fontes <gustavo.ferrazfontes@gmail.com>
2024-11-07 21:40:30 -0300

    feat: create migration to crate post table

--- PostsContext/ImageSharing.Posts.Api/Program.cs
+++ PostsContext/ImageSharing.Posts.Api/Program.cs
@@ -1,3 +1,6 @@
+using ImageSharing.Posts.Infra.EF;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+builder.Services.AddDbContext<PostDbContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("PostContext"))
+);
 
 var app = builder.Build();
 
---
+++ PostsContext/ImageSharing.Posts.Infra/Migrations/20241108003752_PostTable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ImageSharing.Posts.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class PostTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "posts",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    subtitle = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    tags = table.Column<string[]>(type: "text[]", maxLength: 255, nullable: true),
+                    image_path = table.Column<string[]>(type: "text[]", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_posts", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "posts");
+        }
+    }
+}
